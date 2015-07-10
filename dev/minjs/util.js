189468c27fce6bea06713a5040f0ad4ba4a5e4d9
木风 <niluanxy@163.com>
2015-07-10 17:49:36 +0800

    delay函数名错误修改

--- dev/minjs/util.js
+++ dev/minjs/util.js
@@ -10,7 +10,7 @@ module.exports = (function() {
      * @author      mufeng  <smufeng@gmail.com>
      * @version     0.1     <2015-04-10>
      */
-    util.dealyCall = function(func, wait) {
+    util.delayCall = function(func, wait) {
         var context, args, timeout, result;
         var previous = 0;
         var later = function() {
