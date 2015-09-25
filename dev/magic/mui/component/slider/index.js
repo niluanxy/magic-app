79c56bc3d6a117393ac54bf5836cc73b94a9a59e
木风 <niluanxy@163.com>
2015-09-26 02:11:47 +0800

    slider组件优化，添加回调方法

--- dev/magic/mui/component/slider/index.js
+++ dev/magic/mui/component/slider/index.js
@@ -27,9 +27,9 @@ module.exports = (function() {
         that.el.wrapAll("<div class='slider_scroll'></div>");
         scroll = that.el.children();
         childs = scroll.query(".slider-item");
-        $first = $(childs[0] || childs);
+        $first = $(childs[0]);
 
-        this.__render = $first.render(function() {
+        that.__render = $first.render(function() {
             that.pageWidth = $first.width();
             if (opt.scale) {
                 scroll.height(that.pageWidth*opt.scale);
@@ -51,7 +51,7 @@ module.exports = (function() {
                 snapSpeed: 600,
                 scrollbars: false
             });
-            that._initPlay();
+            that.play();
         })
 
         that.el.on("touchstart", function() {
@@ -59,29 +59,56 @@ module.exports = (function() {
         })
 
         that.el.on("touchend", function() {
-            that._initPlay();   // 初始化播放
+            var call = that.options.call;
+            that.play();   // 初始化播放
 
             // 更新当前 slide 停止的页面索引
             var pageX = that.handle.x * -1;
-            that.pageIndex = that.handle.currentPage.pageX;
+            that.pageIndex = parseInt(pageX/that.pageWidth);
+
+            // 尝试手动触发slider载入后的回掉方法
+            if (typeof call == "function") {
+                var index = (that.pageIndex+1);
+                index = index > that.pageLength ? 1 : index;
+                call.call(that, that.el.find(
+                    ".slider-item:nth-child("+index+")"
+                ), index);
+            }
         })
 
         return this;
     };
 
-    Slider.prototype._initPlay = function() {
+    Slider.prototype.play = function(go) {
         if (this.pageWidth > 0) {
-            var that = this, opt = that.options;
+            var that = this, opt  = that.options, call = opt.call;
 
             clearInterval(that.timeHandle);
             if (opt.autoPlay && opt.time) {
                 that.timeHandle = setInterval(function() {
                     that.next();    // 自动播放下一个
+                    if (typeof call == "function") {
+                        var index = (that.pageIndex+1);
+                        index = index > that.pageLength ? 1 : index;
+                        call.call(that, that.el.find(
+                            ".slider-item:nth-child("+index+")"
+                        ), index);
+                    }
                 }, opt.time)
             }
+
+            /* 直接跳转到制定的地方 */
+            if (go && !isNaN(go)) that.go(go);
         }
-        
+
         return this;
+    }
+
+    Slider.prototype.now = function(pos) {
+        var index = this.pageIndex+1,
+            $item = this.el.find(".slider-item:nth-child("+index+")");
+
+        return pos ? index : $item; // 返回下标或者当前元素
     }
 
     Slider.prototype.next = function() {
@@ -90,6 +117,19 @@ module.exports = (function() {
 
     Slider.prototype.prev = function() {
         return this.go(this.pageIndex-1);
+    };
+
+    Slider.prototype.pause = function(time) {
+        var that = this;
+
+        clearInterval(that.timeHandle);
+        if (time && !isNaN(time)) {
+            setTimeout(function() {
+                that.play();
+            }, time)
+        }
+
+        return this;
     };
 
     Slider.prototype.go = function(index) {
@@ -105,7 +145,7 @@ module.exports = (function() {
             this.handle.goToPage(index, 0);     // 滚动到对应页面
             this.pageIndex = index;             // 更新当前索引
         }
-            
+
         return this;
     };
 
