85add887db63e671aff3bdc159ec67ef0cc628c7
木风 <niluanxy@163.com>
2015-10-06 18:35:21 +0800

    路由编写备份_1006

--- dev/minjs/route.js
+++ dev/minjs/route.js
@@ -24,14 +24,25 @@ module.exports = (function() {
 	 *		leave   : function			// 页面离开时的回调
 	 *
 	 *		clear   : false				// 为 true 时清除访问历史，默认 false
-	 *		recusive: false				// URL匹配方式，可覆盖全局设置，默认 false
-	 *
+	 *		recurse : false				// 覆盖全局设置
 	 *
 	 *		'/info' : {...}				// 子路由信息
      *	}
      *
      * */
 
+
+    /* 检测是否为函数 */
+    function isFun(call) {
+        return typeof call == "function"
+    };
+
+    /* 手动触发 PopStateEvent 事件 */
+    function triggerPopEve() {
+        var eve = document.createEvent('PopStateEvent');
+        eve.initEvent("popstate", true, true);
+        window.dispatchEvent(eve);
+    }
 
 
     Route.DEFAULT = {
@@ -42,16 +53,38 @@ module.exports = (function() {
         notpage  : "",                  // 页面未找到的时候，显示的页面
     	before   : null,				// 页面跳转前的回调方法
     	after    : null,				// 页面跳转后的回调方法
-    	recursive: false,				// URL匹配时的方式，默认精确匹配，
-                                        // "forward"正向，"backward"反向
+    	recurse  : false,				// 路由递归触发方式，forward 正序，backward 反序，默认最后项
     }
 
+    /* 执行给定对象的执行方法，forward 为 true 反向执行 */
+    Route.exec = function(tables, key, type) {
+        var len  = tables.length - 1, last = tables[len], ret,
+            back = type == "backward";   // 方法是否反向调用
+
+        if (type === false /* 只执行最后一个对象 */) {
+            if (isFun(last.item[key])) {
+                ret = last.item[key](last.para);
+            }
+        } else {
+            for (var i = back?len:0; back?i>=0:i<=len; back?i--:i++) {
+                var now = tables[i], item = tables[i].item;
+
+                if (isFun(item[key])) {
+                    ret = item[key](now.para);
+                }
+
+                if (ret === false) break; // 返回 false 则终止程序执行
+            }
+        }
+
+        return ret;     // 返回最后执行的结果
+    }
 
     /* 设置当前路由的参数信息，init 为 true 时，初始化模式 */
     Route.prototype.config = function(options, init) {
         options = options ? options : {};   // 修正参数
 
-  		var def = Route.DEFAULT, opt = this.options;
+  		var def = Route.DEFAULT, that = this, opt = that.options;
 
   		for(var key in def) {
   			if (init && def[key] !== undefined) {
@@ -66,7 +99,7 @@ module.exports = (function() {
 
     /* 路由初始化方法，repath 为 true，则跳到首页 */
     Route.prototype.init = function(repath) {
-        var that = this, opt = that.options, tmp;
+        var that = this, opt = that.options, tmp, url;
 
         /* 初始化项目的路由匹配规则 */
         that.table = Route.prefix(that.table);
@@ -77,8 +110,10 @@ module.exports = (function() {
             that.go(opt.home, true, tmp);
         }
 
+        triggerPopEve();        // 手动触发 popstate 事件
+
         /* 初始化上次访问的页面信息 */
-        that.last = that.geturl();
+        that.last = that.fire();
 
         return that;
     }
@@ -87,11 +122,13 @@ module.exports = (function() {
     Route.prefix = function(table) {
         for (var key in table) {
             if (key.search("/") == 0) {
-                /* 添加对应项具体的正则语句 */
-                if (typeof find[key] == "function") {
-                    find[key] = { on: find[key]};   // 修正格式(只传了on方法)
+                
+                /* 修正格式(只传了on方法) */
+                if (isFun(find[key])) {
+                    find[key] = { on: find[key]}
                 }
 
+                /* 添加对应项具体的正则语句 */
                 table[key].__match = key.replace(/:[^_\/-]*/g, "([^/]*)");
                 Route.prefix(table[key]);   // 递归循环处理子项目，直到全部处理完
             }
@@ -100,15 +137,10 @@ module.exports = (function() {
         return table;
     }
 
-    /* 对给定的路由数据进行简化，只保留需要的信息 */ 
-    Route.clear = function(route) {
-
-    }
-
     /* 尝试匹配给定URL的路由信息 */
     Route.prototype.fire = function(url) {
-    	url = url == undefined ? location.hash : url;      // 修正参数
-    	url = url.replace(/^#/, "").replace(/\/$/, "");	   // 替换开头 # 和结尾 /
+    	url = url == undefined ? this.geturl() : url;              // 修正参数
+    	url = "/" + url.replace(/^#/, "").replace(/\/$/, "");	   // 替换开头 # 和结尾 /
 
     	var table = this.table, opt = this.options,
             result = [], tmp, para, key, reg, mat, check;
@@ -144,41 +176,73 @@ module.exports = (function() {
                 check = key;    //  标记当前已经检测过的项目
             }
         } while (url.length > 0 && check != key);
-        	
-        /* 参数修正，如果只有一个项时，直接返回这个项目 */
-        result = result.length > 1 ? result : result[0];
 
     	return url.length == 0 && result ? result : null;
     }
 
-
     /* 全局绑定时间，监控页面前进后退等操作 */
     Route.prototype.bind = function() {
-        var $main = window, that = this, opt = that.options, match, url;
+        var that = this, opt = that.options, change;
 
-        change = window.ontouchstart ? "touchend" : "mouseup";
+        change = window.ontouchstart !== undefined ? "touchend" : "mouseup";
 
-        $main.addEventListener("popstate", function(e) {
-            match = that.fire();    // 获取当前匹配到的页面
-            url   = that.geturl();
-            
-            console.log(e)
-            console.log(url)
-            console.log(match)
-            console.log(that)
+        window.addEventListener("popstate", function(e) {
+            var match = that.fire(), url = that.geturl();
 
-            /* 匹配失败会先尝试跳到notfound页面，无则调回首页 */
-            if (!match /* 匹配失败 */) {
-                
-            } else {
+            if (match /* 只有匹配到数据的时候，才执行操作 */) {
+                Route.exec(match, "before", opt.recurse);
+                Route.exec(match, "on", opt.recurse);
 
+                that.last  = match[match.length-1];
+                that.match = match;
             }
         });
+
+        document.addEventListener(change, function(e) {
+            var target = e.target, tag = target.tagName,
+                href = target.getAttribute("href"), now = that.geturl();
+
+            e.preventDefault(); // 阻止浏览器默认跳转动作
+
+            console.log(change + " has run")
+            if (tag === "A" && href != now) {
+                console.log(change+" has run in")
+                var match = that.fire(href), not, to, rcall;
+
+                not = that.fire(opt.notpage) ? opt.notpage : opt.home;
+                not = that.geturl(not);     // 修复URL格式
+                to  = match ? href : not;   // 设置跳转后的URL
+
+                if (isFun(opt.before)) {
+                    rcall = opt.before(href);
+                }
+
+                /* 如果 before 方法返回false 或 页面未找到，阻止默认跳转 */
+                if (rcall === false || to == not) {
+                    if (to == not && not != now) {
+                        if (isFun(opt.notcall)) {
+                            rcall = opt.notcall(href);
+                        }
+
+                        /* 如果 notcall 返回 false，则不执行跳转到错误页面的动作 */
+                        rcall != false && that.go(not, true, that.fire(not));
+                    }
+                } else {
+                    if (that.last && isFun(that.last.leave)) {
+                        rcall = Route.exec(that.match, "leave", opt.recurse);
+                    }
+
+                    /* 如果上个页面 leave 方法返回 false，则不执行新页面跳转动作 */
+                    rcall != false && that.go(href, false, that.fire(href));
+                }
+            }
+        })
     }
 
-    /* 跳到指定的页面，但不会出发 change 时间 */
-    Route.prototype.go = function(url, replace, state) {
-        var that = this, opt = that.options, call, eve;
+    /* 跳到指定的页面 */
+    Route.prototype.go = function(url, replace, state, slience) {
+        var that = this, opt = that.options, call, eve,
+            last = state && state.length ? state[state.length-1] : null;
 
         if (opt.html5mode) {
             url = url.replace(/^[#,\/]*/g, "/");
@@ -187,24 +251,24 @@ module.exports = (function() {
         }
 
         call = replace ? "replaceState" : "pushState";
-        history[call](state||{}, state && state.title || "", url);
+        history[call](null, last && last.title || "", url);
 
-        /* 手动触发 popstate 事件 */
-        eve = document.createEvent('PopStateEvent');
-        eve.initEvent("popstate", true, true);
-        window.dispatchEvent(eve);
-
+        /* 默认手动触发 popstate 事件 */
+        !slience && triggerPopEve();
+            
         return that;
     };
 
     /* 获取当前URL信息 */
-    Route.prototype.geturl = function() {
+    Route.prototype.geturl = function(url) {
         var that = this, opt = that.options, ret = "";
 
         if (opt.html5mode) {
-            ret = location.pathname;
+            ret = url || location.pathname;
+            ret = ret.replace(/^[#,\/]*/g, "/");
         } else {
-            ret = location.hash;
+            ret = url || location.hash;
+            ret = ret.replace(/^[#,\/]*/g, "#");
         }
 
         return ret;
