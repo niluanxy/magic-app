67d8495621bf84905222a51ad73467594e017551
木风 <niluanxy@163.com>
2016-01-11 18:53:02 +0800

    修改 tap 事件的实现方式，添加对桌面触摸屏的支持

--- dev/magic/mui/component/tap.js
+++ dev/magic/mui/component/tap.js
@@ -6,9 +6,21 @@
  */
 
 $.ready(function() {
-    var $document = $(document), tap = {},
-        delay = 300, fastmove, input;
+    var $document = $(document), tap, fastmove, input;
 
+
+    /* 修复path对象，无则自己模拟一个出来 */
+    function fixPath(e) {
+        var target = e.target, arr = [];
+
+        while (target && target.parentNode) {
+            arr.push(target);   // 当前存下来
+
+            target = target.parentNode;
+        }
+
+        return arr;     // 返回创建的对象
+    }
 
     function checkClass(item, test) {
         var find, cls = item.attr ? item.attr("class") :
@@ -25,23 +37,6 @@ $.ready(function() {
         return false;
     }
 
-    /* 修复input 焦点问题 */
-    $document.on("focus", function(e) {
-        input = e.target;
-    }, true)
-
-    function checkIn(parent, child) {
-        do {
-            if (child == parent) {
-                return true;
-            }
-
-            child = child && child.parentNode;
-        } while (child && child.parentNode);
-
-        return false;
-    }
-
     // 清除激活类
     function clearActive($target) {
         if (checkClass($target)) {
@@ -52,106 +47,146 @@ $.ready(function() {
     }
     $._ui_clearActive = clearActive;
 
-    /* 事件初始化监听 */
-    function faststart(e) {
-        var touchs = e.changedTouches;
-        if (touchs && touchs.length > 1) {
-            tap.startX = null;      tap.startY = null;
-            tap.endX   = null;      tap.endY   = null;
 
-            return true;
-        }
+    tap = {
+        delay  : 300,
+        double : 600,
+        animate: 240,
 
-        var handle, tagName, path = e.path;
+        input: null,
+        target: null,
+        disClick: false,
 
-        /* 记录此次点击事件的相关信息，用于方法判断 */
-        if (touchs && touchs[0]) {
-            tap.startX = touchs[0].pageX;
-            tap.startY = touchs[0].pageY;
-        } else {
-            tap.startX = e.pageX;
-            tap.startY = e.pageY;
-        }
-        tap.startTime = $.getTime();
-        tap.target    = e.target;
+        startX: 0,
+        startY: 0,
+        startTime: 0,
 
-        /* 给按钮类的组件添加点击样式 */
-        for (var i = 0; i<path.length; i++) {
-            var $item = $(path[i]);
-            tagName = $item[0].tagName;
+        _focus: function(e) {
+            this.input = e.target;
+        },
 
-            if (checkClass($item)) {
-                handle = setTimeout((function($actobj) {
-                    return function() {
-                        $actobj.addClass("active");
-                    }
-                })($item), 24);
-                $item.data("_active_handle", handle);
-            }
-        }
-    }
+        _start: function(e) {
+            if ((e.touches && e.touches.length > 1) || 
+                (e.timeStamp - this.startTime < this.double)) {
+                this.startX = null;      this.startY = null;
 
-    /* 节流move事件，节省资源 */
-    fastmove = $.delayCall(function(e) {
-        e.preventDefault(); // 修复微信下拉显示网页地址
-
-        for(var i = 0; i<e.path.length; i++) {
-            var $item = $(e.path[i]);
-            clearActive($item);
-        }
-    }, 16);
-
-    /* 点击结束事件 */
-    function fastend(e) {
-        var cx, cy, ct, $target = $(e.target), tagName,
-            touch = e.changedTouches ? e.changedTouches[0] : e;
-
-        tap.endX = touch.pageX;
-        tap.endY = touch.pageY;
-
-        cx = Math.abs(touch.pageX - tap.startX);
-        cy = Math.abs(touch.pageY - tap.startY);
-        ct = $.getTime() - tap.startTime;
-
-        if (cx<5 && cy < 5 && ct < delay) {
-            $target.trigger("tap");
-
-            /* 修复移动端input点击焦点不更新的问题 */
-            tagName = e.target.tagName.toUpperCase();
-            if (e.target != input) {
-                input && input.blur();      // 刷新焦点
+                return true;
             }
 
-            if ("INPUT TEXTAREA".split(" ").indexOf(tagName) >= 0) {
-                input = e.target;           // 更新绑定元素
+            var touch = e.touches ? e.touches[0] : e,
+                handle, tagName, path = fixPath(e);
 
-                var type = $target.attr("type");
+            /* 记录此次点击事件的相关信息，用于方法判断 */
+            tap.startX = touch.pageX;
+            tap.startY = touch.pageY;
+            tap.startTime = $.getTime();
+            tap.target    = e.target;
 
-                type = type ? type.toUpperCase() : type;
+            /* 给按钮类的组件添加点击样式 */
+            for (var i = 0; i<path.length; i++) {
+                var $item = $(path[i]);
+                tagName = $item[0].tagName;
 
-                if (type && "RADIO CHECKBOX".split(" ").indexOf(type) >= 0) {
-                    if (input.checked == true) {
-                        $target.removeClass("checked");
-                    } else {
-                        if (type == "CHECKBOX") {
-                            $target.addClass("checked");
+                if (checkClass($item)) {
+                    handle = setTimeout((function($actobj) {
+                        return function() {
+                            $actobj.addClass("active");
+                        }
+                    })($item), 24);
+                    $item.data("_active_handle", handle);
+                }
+            }
+        },
+
+        _move: $.delayCall(function(e) {
+            e.preventDefault(); // 修复微信下拉显示网页地址
+
+            var path = fixPath(e);
+
+            for(var i = 0; i<path.length; i++) {
+                var $item = $(path[i]);
+                clearActive($item);
+            }
+        }, 16),
+
+        _end: function(e) {
+            var cx, cy, ct, $target = $(e.target), tagName,
+                touch = e.changedTouches ? e.changedTouches[0] : e;
+
+            cx = Math.abs(touch.pageX - this.startX);
+            cy = Math.abs(touch.pageY - this.startY);
+            ct = $.getTime() - this.startTime;
+
+            if (cx<5 && cy < 5 && ct < this.delay) {
+                $target.trigger("tap");
+
+                /* 修复移动端input点击焦点不更新的问题 */
+                tagName = e.target.tagName.toUpperCase();
+                if (e.target != this.input) {
+                    this.input && this.input.blur();        // 刷新焦点
+                }
+
+                if ("INPUT TEXTAREA".split(" ").indexOf(tagName) >= 0) {
+                    this.input = e.target;                  // 更新绑定元素
+
+                    var type = $target.attr("type");
+
+                    type = type ? type.toUpperCase() : type;
+
+                    if (type && "RADIO CHECKBOX".split(" ").indexOf(type) >= 0) {
+                        if (this.input.checked == true) {
+                            $target.removeClass("checked");
+                        } else {
+                            if (type == "CHECKBOX") {
+                                $target.addClass("checked");
+                            }
                         }
                     }
                 }
+
+                this.disClick = false;
+                $target.trigger("click");
             }
 
-            tap.disClick = false;
-            $target.trigger("click");
+            /* 如果点击时间太短，手动延迟动画移除时间 */
+            var _animate = this.animate;
+            setTimeout(function() {
+                var path = fixPath(e);
+
+                for(var i = 0; i<path.length; i++) {
+                    var $item = $(path[i]);
+                    clearActive($item);
+                }
+            }, _animate-ct >= 0 ? _animate-ct : 0);
+        },
+
+        handleEvent: function (e) {
+            switch ( e.type ) {
+                case 'touchstart':
+                case 'pointerdown':
+                case 'MSPointerDown':
+                case 'mousedown':
+                    this._start(e);
+                    break;
+                case 'touchmove':
+                case 'pointermove':
+                case 'MSPointerMove':
+                case 'mousemove':
+                    this._move(e);
+                    break;
+                case 'touchend':
+                case 'pointerup':
+                case 'MSPointerUp':
+                case 'mouseup':
+                case 'touchcancel':
+                case 'pointercancel':
+                case 'MSPointerCancel':
+                case 'mousecancel':
+                    this._end(e);
+                    break;
+            }
         }
-
-        /* 如果点击时间太短，手动延迟动画移除时间 */
-        setTimeout(function() {
-            for(var i = 0; i<e.path.length; i++) {
-                var $item = $(e.path[i]);
-                clearActive($item);
-            }
-        }, 220-ct >= 0 ? 220-ct : 0);
-    }
+    };
 
 
     /** 
@@ -168,20 +203,32 @@ $.ready(function() {
      * 之后延迟触发的，浏览器会误认为是在遮盖的元素上触发了click。
      * 
      */
-    (function() {
-        var ename, start, move, end, kt,
-            map = { s: ["start", "down"], e : ["end", "up"] };
+    ;(function() {
+        var kt = window.ontouchstart !== undefined ? 0 : 1,
+            docbind = document.addEventListener;
 
-        kt = window.ontouchstart !== undefined ? 0 : 1;
+        /* 修复input 焦点问题 */
+        docbind("focus", tap._focus, true)
 
-        ename = kt ? "mouse" : "touch";
-        start = ename + map.s[kt];
-        move  = ename + "move";
-        end   = ename + map.e[kt];
+        docbind("mousedown", tap);
+        docbind("mousemove", tap);
+        docbind("mouseup", tap);
+        docbind("mousecancel", tap);
 
-        $document.on(start, faststart);
-        $document.on(move,  fastmove);
-        $document.on(end,   fastend);
+        docbind("touchstart", tap);
+        docbind("touchmove", tap);
+        docbind("touchend", tap);
+        docbind("touchcancel", tap);
+
+        docbind("pointerdown", tap);
+        docbind("pointermove", tap);
+        docbind("pointerup", tap);
+        docbind("pointercancel", tap);
+
+        docbind("MSPointerDown", tap);
+        docbind("MSPointerMove", tap);
+        docbind("MSPointerUp", tap);
+        docbind("MSPointerCancel", tap);
 
         /* 修复鼠标点透问题 */
         if (kt === 0 /* 只有支持 touch 事件时才绑定 */) {
