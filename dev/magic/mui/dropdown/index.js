83a129a4e0300c7e507c47c8365f14b2b313d757
木风 <niluanxy@163.com>
2016-10-11 17:57:02 +0800

    弹框组件修改

--- dev/magic/mui/dropdown/index.js
+++ dev/magic/mui/dropdown/index.js
@@ -134,6 +134,7 @@ module.exports = (function() {
 
     Dropdown.prototype.toggle = function(show) {
         show = show === undefined ? !this.visible : !!show;
+        this.$el.place(this.$wrap, "bottom", "left", {float: true});
         this.$wrap.toggleClass("hide", !show);
         this.visible = show;
 
--- dev/magic/mui/place/index.js
+++ dev/magic/mui/place/index.js
@@ -1,11 +1,11 @@
 exports.module = (function(doc, undefined) {
-	var OFFSET = {
-		'top'    : 0,
-		'left'   : 0,
-		'center' : 0.5,
-		'right'  : 1,
-		'bottom' : 1
-	};
+    var OFFSET = {
+        'top'    : 0,
+        'left'   : 0,
+        'center' : 0.5,
+        'right'  : 1,
+        'bottom' : 1
+    };
 
     /**
      * 相对某元素执行定位操作
@@ -13,10 +13,19 @@ exports.module = (function(doc, undefined) {
      * @param {Element}     relative    [相对定位的元素]
      * @param {String}      top         [上下定位参数及修正]
      * @param {String}      left        [左右定位参数及修正]
-     * @param {Number}      zindex      [定位元素的 z-index 值]
      *
-     * inner 是否在定位元素外插入对象，默认为元素内插入，如果选择元
-     * 素外插入，计算坐标时，会自动加上定位元素的偏移值
+     * @param {Boolean}     outer       [是否元素外部插入]
+     * @param {Number}      zindex      [定位元素的 z-index 值]
+     * @param {Boolean}     float       [显示不下时是否自动浮动]
+     *
+     * option : {
+     *     outer:     false [default],
+     *     float:     false [default],
+     *     zindex:    100   [default],
+     * }
+     * 
+     *
+     * outer 是否在定位元素外插入对象，默认为元素内插入
      *
      * top : ["top offset",  "center offset", "bottom offset"]
      * left: ["left offset", "center offset", "right offset"]
@@ -30,18 +39,23 @@ exports.module = (function(doc, undefined) {
      * 宽度的 0.5 倍，即向右偏移定位元素 0.5倍 的高度距离;
      *
      */
-	var Place = function (el, relative, top, left, outer, zindex) {
-		if (!el || !relative || !top || !left) return;
+    var Place = function (el, relative, top, left, option) {
+        if (!el || !relative || !top || !left) return;
 
         // 如果是 $ 对象，修复对象
         el = el instanceof Magic ? el[0] : el;
+        option = option ? option : {};
+
         relative = relative[0] ? relative[0] : relative;
+        relative = $(relative)[0] || document.body;
 
-		zindex = zindex || Place.zindex || 100;
-		relative = $(relative)[0] || document.body;
+        var parent, offset, $el = $(el), place = [],
+            topfix, leftfix, zindex, outer, float,
+            rel = Place.outerRect(relative);
 
-		var parent, offset, $el = $(el), style, place = [],
-            topfix, leftfix, rel = Place.outerRect(relative);
+        outer = !!option.outer;
+        float = !!option.float;
+        zindex = option.zindex || Place.zindex || 100;
 
         if (outer === true) {
             parent = Place.relRoot(relative);
@@ -57,8 +71,8 @@ exports.module = (function(doc, undefined) {
             $(parent).css("position", "relative");
         }
 
-		place[0] = top.split(" ");
-		place[1] = left.split(" ");
+        place[0] = top.split(" ");
+        place[1] = left.split(" ");
 
         // 如果实际定位的元素不是元素自身，需要修正坐标值
         if (parent != relative) {
@@ -69,26 +83,48 @@ exports.module = (function(doc, undefined) {
             offset.left -= tmp.left;
 
             offset.top  += OFFSET[place[0][0]] * rel.height;
-    		offset.left += OFFSET[place[1][0]] * rel.width;
+            offset.left += OFFSET[place[1][0]] * rel.width;
         } else {
             offset = {};
 
             offset.top  = OFFSET[place[0][0]] * rel.height;
-    		offset.left = OFFSET[place[1][0]] * rel.width;
+            offset.left = OFFSET[place[1][0]] * rel.width;
         }
 
-		topfix  = parseFloat(place[0][1]) || 0;
-		leftfix = parseFloat(place[1][1]) || 0;
+        topfix  = parseFloat(place[0][1]) || 0;
+        leftfix = parseFloat(place[1][1]) || 0;
 
-		offset.top  += ele.height * topfix;
-		offset.left += ele.width  * leftfix;
+        offset.top  += ele.height * topfix;
+        offset.left += ele.width  * leftfix;
 
-		style  = $el.attr("style") || '';
-        style += "top:"+offset.top+"px;left:"+offset.left+"px;"
-                +"z-index:"+zindex+";position:absolute;";
-		$el.attr("style", style);
-		$el.appendTo(parent);
-	};
+        // 如果设置了自动浮动，检测是否超出显示
+        if (float === true) {
+            var screen = Place.outerRect(document),
+                fwidth = ele.width - rel.width,
+                fheight= ele.height + rel.height,
+                pleft  = place[1][0] == "left",
+                pbottom= place[0][0] == "bottom";
+
+            if (pleft && rel.left + ele.width > screen.width) {
+                offset.left -= fwidth;
+            } else if (!pleft && rel.left - fwidth < 0) {
+                offset.left += fwidth;
+            }
+
+            if (pbottom && rel.top + fheight > screen.height) {
+                offset.top -= fheight;
+            } else if (!pbottom && offset.top < 0 && rel.top - ele.height < 0) {
+                offset.top += fheight;
+            }
+        }
+
+        $el.css("top", offset.top+"px");
+        $el.css("left", offset.left+"px");
+        $el.css("z-index", zindex);
+        $el.css("position", "absolute");
+
+        $el.appendTo(parent);
+    };
 
     /**
      * 获取元素的 尺寸信息
@@ -102,84 +138,87 @@ exports.module = (function(doc, undefined) {
      * relative 为要插入的容器，因为有时候插入的元素会收到
      * 容器CSS的影响，导致尺寸有变化，为空默认为body元素
      */
-	Place.outerRect = function(el, relative) {
-		relative = $(relative)[0] || document.body;
+    Place.outerRect = function(el, relative) {
+        relative = $(relative)[0] || document.body;
 
-		var rect, copy = {}, clone, fix, docElem, win = window,
-			render = '<div style="position: absolute; visibility: hidden"></div>';
+        var rect, copy = {}, clone, fix, docElem, win = window,
+            render = '<div style="position: absolute; visibility: hidden"></div>';
 
-		if (el == document) {
-			var body = document.body,
-				width = body.clientWidth,
-				height = body.clientHeight;
+        if (el == document) {
+            var body = document.body,
+                width = body.clientWidth,
+                height = body.clientHeight;
 
-			rect = {
-				top: 0, left: 0, right: width,
+            rect = {
+                top: 0, left: 0, right: width,
 
-				bottom: height, width: width, height: height
-			}
-		} else {
-			if (typeof el == "string") {
-				rect  = {top: 0, left: 0, right: 0, bottom: 0};
-				clone = el;
-			} else if (el.getBoundingClientRect) {
-				clone = el.cloneNode(true);
-				rect  = el.getBoundingClientRect();
+                bottom: height, width: width, height: height
+            }
+        } else {
+            if (typeof el == "string") {
+                rect  = {top: 0, left: 0, right: 0, bottom: 0};
+                clone = el;
+            } else if (el.getBoundingClientRect) {
+                clone = el.cloneNode(true);
+                rect  = el.getBoundingClientRect();
                 docElem = el.ownerDocument.documentElement;
-			}
+            }
 
-			if (!rect.width && !rect.height && relative) {
-				render = $(render).appendTo(relative);
+            if (!rect.width && !rect.height && relative) {
+                render = $(render).appendTo(relative);
 
                 el = $(clone).appendTo(render)[0];
                 $(el).css("position: absolute;");
-				fix = el.getBoundingClientRect();
+                $(el).removeClass("hide");
+                fix = el.getBoundingClientRect();
                 docElem = el.ownerDocument.documentElement;
 
-				render.remove();	// 删除创建的零时节点
-			}
-		}
+                render.remove();    // 删除创建的零时节点
+            }
+        }
 
-		for (var key in rect) {
-			copy[key] = rect[key];
-		}
+        for (var key in rect) {
+            copy[key] = rect[key];
+        }
 
-		if (fix /* 修复对象为真，则修复高宽 */) {
-			copy.width  = fix.width;
-			copy.height = fix.height;
-		}
+        if (fix /* 修复对象为真，则修复高宽 */) {
+            copy.width  = fix.width;
+            copy.height = fix.height;
+        }
 
-        copy.top  = copy.top + win.pageYOffset - docElem.clientTop;
-        copy.left = copy.left + win.pageXOffset - docElem.clientLeft;
+        if (docElem && docElem.clientTop !== undefined) {
+            copy.top  = copy.top + win.pageYOffset - docElem.clientTop;
+            copy.left = copy.left + win.pageXOffset - docElem.clientLeft;
+        }
 
-		return copy;
-	}
+        return copy;
+    }
 
-	Place.relRoot = function(el, out) {
-		var relative, node = el;
+    Place.relRoot = function(el, out) {
+        var relative, node = el;
 
-		do {
-			if (out && node == el) continue;
+        do {
+            if (out && node == el) continue;
 
-			if (getComputedStyle(node).position != "static") {
-				relative = node; break;
-			}
-		} while(node = el.parentNode);
+            if (getComputedStyle(node).position != "static") {
+                relative = node; break;
+            }
+        } while(node = el.parentNode);
 
-		return relative || document.body;
-	}
+        return relative || document.body;
+    }
 
-	/* 尝试绑定方法到 magic 框架的全局对象上 */
+    /* 尝试绑定方法到 magic 框架的全局对象上 */
     if ($ && !$.place) {
         $.extend({place: Place});
         $.extend({outerRect: Place.outerRect});
     };
 
     if ($ && $.fn && !$.fn.place) {
-        $.fn.extend({place: function(el, top, left, zindex) {
-            Place(el, this, top, left, zindex);
+        $.fn.extend({place: function(el, top, left, option) {
+            Place(el, this, top, left, option);
 
-			return this;
+            return this;
         }});
 
         $.fn.extend({outerRect: function(relative) {
